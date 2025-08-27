f7991f09133e875fded0f8cfcd781ecda7525f69
lf002 <lukefreyn@gmail.com>
2025-08-27 10:16:33 -0400

    jef

---
+++ Week 2/calculator.py
@@ -0,0 +1,26 @@
+var_1 = input("Please enter your first number:")
+var_2 = input("Please enter your second number:")
+
+
+var_1 = float(var_1)
+var_2 = float(var_2)
+
+
+
+# Addition
+sum = var_1 + var_2
+print(f"{var_1} + {var_2} = {sum}")
+
+# Subtraction
+difference = var_1-var_2
+print(f"{var_1} - {var_2} = {difference}")
+
+# Product
+product = var_1 * var_2
+print(f"{var_1} * {var_2} = {product}")
+
+# Division
+quotient = var_1 / var_2
+print(f"{var_1} / {var_2} = {quotient}")
+
+
---
+++ Week 2/mathymath.py
@@ -0,0 +1,40 @@
+# Let's do some math.
+
+# Addition
+print(3+5-2+5)
+
+# Subtraction
+print(587465345-12987123)
+
+# Multlipication
+print(3 * 5)
+
+# Division
+print (3/5)
+
+# Exponents **
+print(3**5)
+
+# Modulo - This is the remainder after you divide a number. drop the number output remainder.
+print(5%3)
+print(13%2)
+print(25%6)
+print(1231231%4)
+
+
+# Increments ++ & Decrements --
+x = 1
+print(x)
+x += 1
+print(x)
+x += x
+print(x)
+x = x+1
+print(x)
+
+y = 100
+print(y)
+y -= 10
+print(y)
+y -= y + 50
+print(y)
