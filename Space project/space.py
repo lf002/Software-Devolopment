cb657c2ba8fabea5993417e6392e085898d94604
lf002 <lukefreyn@gmail.com>
2025-09-15 08:57:20 -0400

    made a weight calculator for planets

---
+++ Space project/space.py
@@ -0,0 +1,34 @@
+weight = float(input("Please Enter Your Weight: "))
+
+
+mercury_weight = weight * 0.90
+venus_weight = weight * 0.91
+mars_weight = weight * 0.38
+jupiter_weight = weight * 2.34
+saturn_weight = weight * 1.06
+uranus_weight = weight * 0.92
+neptune_weight = weight * 1.19
+pluto_weight = weight * 0.063
+
+print("Your weight on mercury is", mercury_weight)
+print("Your weight on venus is", venus_weight)
+print("Your weight on mars is", mars_weight)
+print("Your weight on jupiter us", jupiter_weight)
+print("Your weight on saturn is", saturn_weight)
+print("Your weight on uranus is", uranus_weight)
+print("Your weight on neptune is", neptune_weight)
+print("Your weight on pluto is", pluto_weight)
+
+print("Have a good day")
+
+
+
+
+
+
+
+
+
+
+
+
