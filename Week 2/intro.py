131ce54126c2d67ffa0df59a7c46037eb04363d3
lf002 <lukefreyn@gmail.com>
2025-09-23 10:13:45 -0400

    Add files via upload

---
+++ Week 2/intro.py
@@ -0,0 +1,19 @@
+#Below are my notes for coding.
+#Here are more practice notes.
+
+# Print Statement - This is used to output to the Terminal AKA Console.
+print('Mr. Sekol is the coolest teacher in the galaxy. Ask Mr. Wright.')
+
+#Variables - These can change value based on user input or you can just assign a value to them. 
+player_name = "Steve"
+print(player_name)
+#Concatenation = Combine String + Variables to form an output.
+print(f"Welcome, {player_name}, it's great to have you here.")
+#Pass through the variable into the output
+player_name = "Bob"
+print(f"Welcome, {player_name}, it's great to have you here.")
+
+
+#User input - create a variable and ask user for input.
+country_of_origin = input(f"Well {player_name}, can you tell me the country you're from? ")
+print(f"{player_name} is from {country_of_origin}. ")
