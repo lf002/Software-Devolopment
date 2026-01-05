8dddfd9809b394a7d35e8bfcd22d4c68222bec5a
Freyn, Lucas <lukefreyn@gmail.com>
2026-01-05 10:09:02 -0500

    Add files via upload

---
+++ products.py
@@ -0,0 +1,23 @@
+class Product:
+    def __init__ (self, _name, _sku, _price):
+        self.__name = _name
+        self.__sku = _sku
+        self.__price = _price
+
+def get_name(self): 
+     return self._name
+
+def get_name(self): 
+     return self._sku
+
+def get_name(self): 
+     return self._price
+
+class Laptop(Product):
+    def __init__ (self, _ram, _storage, _touchscreen):
+        self._ram = _ram
+        self._storage =_storage
+        self._touchscreen = _touchscreen
+
+
+
