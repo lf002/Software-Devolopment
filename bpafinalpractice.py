9deb4a3f354b7fec625efe97a87954f2cf211b5e
Freyn, Lucas <lukefreyn@gmail.com>
2026-01-08 09:50:16 -0500

    Add files via upload

---
+++ bpafinalpractice.py
@@ -0,0 +1,29 @@
+# Freyn, Lucas
+
+squads = int(input("How many squads are you taking into the mission, must be 1-5,")) 
+if 1 <= squads <= 5:
+ print(f"Continue")
+else:
+ print("Error. The number must be through 1 and 5.") 
+
+ if squads  
+
+print("Now you must enter names for the amount of squads you entered.")
+
+squadname1 = input("What is the name of your first squad?")
+
+squadname2 = input("What is the name of your second squad?")
+
+squadname3 = input("What is the name of your third squad?")
+
+squadname4 = input("What is the name of your fourth squad?")
+
+squadname5 = input("What is the name of your fifth squad?")
+
+
+
+
+
+
+
+
